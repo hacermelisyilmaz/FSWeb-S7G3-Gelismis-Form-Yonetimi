78ac4bf81acaba1b9abc34b05bff178ef176fb85
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-08-09 17:11:55 +0300

    React app is built and Yup, Reactstrap, and Bootstrap are installed and imported

---
+++ kullanici-katilimi/src/App.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+import Form from "./components/Form.js";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Form />
+    </div>
+  );
+}
+
+export default App;
---
+++ kullanici-katilimi/src/components/Form.js
@@ -0,0 +1,12 @@
+import * as Yup from "yup";
+import { Button } from "reactstrap";
+
+function Form() {
+  return (
+    <div className="Form">
+      <form></form>
+    </div>
+  );
+}
+
+export default Form;
