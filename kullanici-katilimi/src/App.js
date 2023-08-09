52ba83eb5e88b0700001087fd4b17a1c0fabdb7e
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-08-09 22:19:28 +0300

    Yup is handled

--- kullanici-katilimi/src/App.js
+++ kullanici-katilimi/src/App.js
@@ -1,24 +1,10 @@
-import axios from "axios";
 import FormComponent from "./components/FormComponent.js";
 import "./App.css";
-import { useEffect, useState } from "react";
 
 function App() {
-  const [userList, setUserList] = useState([]);
-
-  const addUser = (newUser) => {
-    const updatedUserList = [...userList];
-    updatedUserList.push(newUser);
-    setUserList(updatedUserList);
-  };
-
-  useEffect(() => {
-    console.log("user list:", userList);
-  }, [userList]);
-
   return (
     <div className="App">
-      <FormComponent pAddUser={addUser} />
+      <FormComponent />
     </div>
   );
 }
--- kullanici-katilimi/src/components/FormComponent.js
+++ kullanici-katilimi/src/components/FormComponent.js
@@ -1,8 +1,16 @@
-import { useState } from "react";
-import { Button, Form, FormGroup, Input, Label } from "reactstrap";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import {
+  Button,
+  Form,
+  FormFeedback,
+  FormGroup,
+  Input,
+  Label,
+} from "reactstrap";
 import * as Yup from "yup";
 
-function FormComponent({ pAddUser }) {
+function FormComponent() {
   const emptyForm = {
     name: "",
     email: "",
@@ -13,23 +21,21 @@ function FormComponent({ pAddUser }) {
   const [formData, setFormData] = useState(emptyForm);
   const [formErrors, setFormErrors] = useState(emptyForm);
   const [isFormValid, setFormValid] = useState(false);
-
-  const changeHandler = (event) => {
-    const { name, value, type, checked } = event.target;
-    setFormData({
-      ...formData,
-      [name]: type === "checkbox" ? checked : value,
-    });
-  };
-
-  const submitHandler = (event) => {
-    event.preventDefault();
-    pAddUser(formData);
-    setFormData(emptyForm);
-  };
+  const [userList, setUserList] = useState([]);
 
   const formSchema = Yup.object().shape({
-    name: Yup.string(),
+    name: Yup.string()
+      .required("Lütfen isminiiz ve soyisminizi giriniz.")
+      .test(
+        "min-words",
+        "Girdiniz en az iki kelimeden oluşmalıdır.",
+        (value) => {
+          if (value) {
+            const words = value.split(" ");
+            return words.length >= 2;
+          }
+        }
+      ),
     email: Yup.string()
       .email("Lütfen geçerli bir mail adresi girin.")
       .required("Bir mail adresi girmelisiniz."),
@@ -42,6 +48,50 @@ function FormComponent({ pAddUser }) {
     ),
   });
 
+  const validateFormField = (event) => {
+    const { name, value, type, checked } = event.target;
+    const input = type === "checkbox" ? checked : value;
+
+    Yup.reach(formSchema, name)
+      .validate(input)
+      .then((valid) => {
+        setFormErrors({ ...formErrors, [name]: "" });
+      })
+      .catch((error) => {
+        setFormErrors({ ...formErrors, [name]: error.errors[0] });
+      });
+  };
+
+  const changeHandler = (event) => {
+    const { name, value, type, checked } = event.target;
+    validateFormField(event);
+    setFormData({
+      ...formData,
+      [name]: type === "checkbox" ? checked : value,
+    });
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    axios
+      .post("https://reqres.in/api/users", formData)
+      .then(function (response) {
+        const updatedUserList = [...userList];
+        updatedUserList.push(response.data);
+        setUserList(updatedUserList);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+    setFormData(emptyForm);
+  };
+
+  useEffect(() => {
+    formSchema.isValid(formData).then((valid) => setFormValid(valid));
+  }, [formData]);
+
   return (
     <div className="Form" onSubmit={submitHandler}>
       <Form>
@@ -53,7 +103,9 @@ function FormComponent({ pAddUser }) {
             placeholder="İsim ve soyisminizi girin"
             onChange={changeHandler}
             value={formData.name}
+            invalid={!!formErrors.name}
           ></Input>
+          <FormFeedback>{formErrors.name}</FormFeedback>
         </FormGroup>
         <FormGroup>
           <Label htmlFor="user-email">E-mail</Label>
@@ -64,7 +116,9 @@ function FormComponent({ pAddUser }) {
             type="email"
             onChange={changeHandler}
             value={formData.email}
+            invalid={!!formErrors.email}
           ></Input>
+          <FormFeedback>{formErrors.email}</FormFeedback>
         </FormGroup>
         <FormGroup>
           <Label htmlFor="user-password">Şifre</Label>
@@ -75,7 +129,9 @@ function FormComponent({ pAddUser }) {
             type="password"
             onChange={changeHandler}
             value={formData.password}
+            invalid={!!formErrors.password}
           ></Input>
+          <FormFeedback>{formErrors.password}</FormFeedback>
         </FormGroup>
         <FormGroup check>
           <Input
@@ -84,13 +140,17 @@ function FormComponent({ pAddUser }) {
             type="checkbox"
             onChange={changeHandler}
             checked={!!formData.terms}
+            invalid={!!formErrors.terms}
           ></Input>
           <Label htmlFor="user-terms">
             Kullanım şartlarını okudum, kabul ediyorum.
           </Label>
+          <FormFeedback>{formErrors.terms}</FormFeedback>
         </FormGroup>
         <FormGroup>
-          <Button type="submit">Submit</Button>
+          <Button type="submit" disabled={!isFormValid}>
+            Gönder
+          </Button>
         </FormGroup>
       </Form>
     </div>
