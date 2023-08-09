bdcd985aa11b36b93c0ad56bb24ea4538e783142
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-08-09 17:17:08 +0300

    FormComponent: An empty object of form parameters is created

--- kullanici-katilimi/src/components/FormComponent.js
+++ kullanici-katilimi/src/components/FormComponent.js
@@ -2,6 +2,13 @@ import * as Yup from "yup";
 import { Form } from "reactstrap";
 
 function FormComponent() {
+  const emptyForm = {
+    name: "",
+    email: "",
+    password: "",
+    terms: "",
+  };
+
   return (
     <div className="Form">
       <Form></Form>
