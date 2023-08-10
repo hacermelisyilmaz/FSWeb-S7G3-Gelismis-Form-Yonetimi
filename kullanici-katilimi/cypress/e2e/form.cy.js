2fd0d8b4d88742eae0bf1dd0da1e0933fb92bcd5
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-08-10 21:09:59 +0300

    Name Input is described to type a name and check if the name is written

--- kullanici-katilimi/cypress/e2e/form.cy.js
+++ kullanici-katilimi/cypress/e2e/form.cy.js
@@ -3,3 +3,11 @@ describe("The Home Page", () => {
     cy.visit("/"); // change URL to match your dev URL
   });
 });
+
+describe("Name Input", () => {
+  it("checks if the name is 'Melis Yılmaz'", () => {
+    cy.visit("/");
+    cy.get("[data-cy=name-input]").type("Melis Yılmaz");
+    cy.get("[data-cy=name-input]").should("have.value", "Melis Yılmaz");
+  });
+});
